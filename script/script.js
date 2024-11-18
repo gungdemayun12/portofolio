ef95c3ec31b47c59a88e7ae1a2c75c1563be140a
gungdemayun12 <gungdemayun64@gmail.com>
2024-11-18 13:01:32 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const closeIcon = document.querySelector('.close-icon');
+
+menuIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('open'); 
+    
+});
+
+
+
+
+
+
+
